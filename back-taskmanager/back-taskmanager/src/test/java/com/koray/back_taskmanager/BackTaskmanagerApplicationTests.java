d983e27bfe4e03a199e3b19672616f5161d540ee
DeveloperKoray <koraykutlu@hotmail.be>
2025-08-14 13:19:41 +0200

    First commit created two project. Java spring for back end and angular for front. Never used angular with git i hope there will no problem.

---
+++ back-taskmanager/back-taskmanager/src/test/java/com/koray/back_taskmanager/BackTaskmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koray.back_taskmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackTaskmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
